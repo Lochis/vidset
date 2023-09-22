66802a806811f22a1950ca31c9cb05cef772a5d7
Lukowie <lukevdm64@gmail.com>
2023-09-22 16:29:12 -0400

    cleaning up

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,7 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/callback"
+    callbackURL: "http://localhost:8000/auth/callback"
   },
   function(accessToken, refreshToken, profile, done) {
       userProfile=profile;
@@ -55,4 +55,3 @@ passport.serializeUser(function(user, cb) {
 
 
 module.exports = router;
- 
